d1b35ed8177f6c5f08f0a32bb7df48fe25b43123
Luis Garces <juan.perez@ejemplo.com>
2025-12-03 10:48:17 -0400

    Ejercicio objetos

---
+++ ejercicioObjetoEstudiante.js
@@ -0,0 +1,22 @@
+let estudiante = {
+    nombre: "luis",
+    edad: "25",
+    notas: [17,16,20,11],
+
+    calcularPromedio: function () { 
+        let sumtotal = 0;
+        this.notas.forEach(nota => {
+            sumtotal += nota;
+        });
+        let promedio = sumtotal / this.notas.length;
+        return promedio;
+    },
+
+    mostrarInfo: function(){
+        return `El estudiante ${this.nombre} tiene un promedio de: ${this.calcularPromedio()}`;
+    }
+}
+
+console.log(estudiante.calcularPromedio());
+console.log(estudiante.mostrarInfo());
+
