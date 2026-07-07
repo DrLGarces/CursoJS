cf3901acffb95eb0bf57325bbe92a21bb897caee
Luis Garces <juan.perez@ejemplo.com>
2026-07-07 12:23:21 -0400

    Envío de data a las tablas

--- ProyectoPresupuesto/js/Movimiento.js
+++ ProyectoPresupuesto/js/Movimiento.js
@@ -3,16 +3,17 @@
 export default class Movimiento{
     static _ingresos = 0;
     static _egresos = 0;
+    static contadorMovimientos = 0;
     constructor(tipo, descripcion, monto){
+        this._id = Movimiento.contadorMovimientos++;
         this._tipo = tipo;
         this._descripcion = descripcion;
         this._monto = Number(monto);
         if(tipo == "Ingreso"){
             Movimiento._ingresos += monto;
         }else if(tipo == "Egreso"){
-            Movimiento._egresos +=monto;
+            Movimiento._egresos += monto;
         }
-
     }
     static get saldoActual(){
         return Movimiento.ingresos - Movimiento.egresos;
@@ -25,7 +26,21 @@ export default class Movimiento{
     static get egresos(){
         return Movimiento._egresos;
     }
+
+    get Id(){
+        return this._id;
+    }
     
+    get Descripcion(){
+        return this._descripcion;
+    }
+
+    get Monto(){
+        return this._monto;
+    }
+    get Tipo(){
+        return this._tipo;
+    }
 
 }
 
--- ProyectoPresupuesto/js/app.js
+++ ProyectoPresupuesto/js/app.js
@@ -5,6 +5,9 @@ const contenedorIngresos = document.querySelector(".contenedor-ingresos");
 const contenedorEgresos = document.querySelector(".contenedor-egresos");
 const formMovimientos = document.getElementById("form-movimiento");
 const btnAgregar = document.getElementById("btnAgregar");
+const tablaIngresos = document.getElementById("tabla-ingresos");
+const tablaEgresos = document.getElementById("tabla-egresos");
+let listaMovimientos = [];
 
 const actualizar = () =>{
     let formatoSaldoActual = `$${Movimiento.saldoActual}`;
@@ -15,6 +18,7 @@ const actualizar = () =>{
 
     let formatoEgresos = `$${Movimiento.egresos}`;
     contenedorEgresos.innerHTML = formatoEgresos;
+    
 }
 
 actualizar();
@@ -26,6 +30,25 @@ btnAgregar.addEventListener("click",(e)=>{
     let descripcion = data.get("descripcion");
     let monto = Number(data.get("monto"));
     let movimiento = new Movimiento(tipo, descripcion, monto);
+    listaMovimientos.push(movimiento);
+    tablaIngresos.innerHTML = "";
+    tablaEgresos.innerHTML = "";
+    
+    listaMovimientos.forEach(mov => {
+    let linea =
+    `<tr>
+        <td class="col-descripcion">${mov.Descripcion}</td>
+        <td class="col-monto">${mov.Monto}</td>
+        <td>
+            <button class="btn btn-sm btn-outline-danger btn-eliminar">❌</button>
+        </td>
+    </tr>`;
+        if(mov.Tipo =="Ingreso"){
+            tablaIngresos.innerHTML += linea;
+        }else if(mov.Tipo == "Egreso"){
+            tablaEgresos.innerHTML += linea;
+        }
+    });
     actualizar();
 });
 
